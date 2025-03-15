b8eaa4622d7c918d3da71020eec65260e8fbe4ac
[daniamdrei] <[daniamdrei@gmail.com]>
2025-03-15 17:56:48 +0300

    create all models

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'food_item_id',
+        'quantity',
+        'price',
+    ];
+    public function foodItem():BelongsTo
+    {
+        return $this->belongsTo(FoodItem::class);
+    }
+
+    public function order():BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
